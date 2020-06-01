1f7059de50a28405f0d4d27d0f4fb65ba121d4c6
jlufrano <47507303+jlufrano@users.noreply.github.com>
2020-06-01 17:17:22 -0400

    Initial reverse of flashcard functionality.

--- app/src/main/java/edu/bu/metcs/activitylifecycle/FlashcardActivity.java
+++ app/src/main/java/edu/bu/metcs/activitylifecycle/FlashcardActivity.java
@@ -10,15 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class FlashcardActivity extends AppCompatActivity {
 
     private  Alphabet alphabet;  // Loads class that manages English alphabet
-    private ImageView flashcardGraphic; // Holds graphic for hand sign
-    private TextView answerText; // Holds display text that corresponds with hand sign graphic
+    private ImageView handSignGraphic; // Holds graphic for hand sign
+    private TextView englishLetter; // Holds display text that corresponds with hand sign graphic
     private Button answerButton, nextCardButton; // Button objects for advancing
+    private Switch letterSwitch;
+    private boolean letterFirst;
 
     /**
      * Initializes needed variables.
@@ -28,12 +32,30 @@ public class FlashcardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
         alphabet = new Alphabet();
-        flashcardGraphic = (ImageView) findViewById(R.id.flashcard);
-        answerText = (TextView) findViewById(R.id.answerText);
-        answerText.setVisibility(View.INVISIBLE);
+        handSignGraphic = (ImageView) findViewById(R.id.flashcard);
+        englishLetter = (TextView) findViewById(R.id.answerText);
+        englishLetter.setVisibility(View.INVISIBLE);
         answerButton = (Button) findViewById(R.id.answerButton);
         nextCardButton = (Button) findViewById(R.id.next_card_button);
-        nextCardButton.setVisibility(View.GONE);
+        nextCardButton.setVisibility(View.INVISIBLE);
+        letterSwitch = (Switch) findViewById(R.id.letterSwitch);
+
+        letterSwitch.setOnCheckedChangeListener((new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on){
+                if(on)
+                {
+                    //Do something when Switch button is on/checked
+                    letterFirst = true;
+                }
+                else
+                {
+                    //Do something when Switch is off/unchecked
+                    letterFirst = false;
+                }
+            }
+        }));
+
         setUpFlashCard();
     }
 
@@ -41,29 +63,45 @@ public class FlashcardActivity extends AppCompatActivity {
      * Advances to English letter that corresponds with shown hand sign.
      */
     public void onClickRevealAnswer(View view) {
-        flashcardGraphic.setVisibility(View.INVISIBLE);
-        answerText.setVisibility(View.VISIBLE);
-        answerButton.setVisibility(View.GONE);
-        nextCardButton.setVisibility(View.VISIBLE);
+        if(letterFirst) { //Letter First Selected
+            handSignGraphic.setVisibility(View.VISIBLE);
+            englishLetter.setVisibility(View.INVISIBLE);
+            answerButton.setVisibility(View.VISIBLE);
+            nextCardButton.setVisibility(View.INVISIBLE);
+        } else { //Sign First (Default)
+            handSignGraphic.setVisibility(View.INVISIBLE);
+            englishLetter.setVisibility(View.VISIBLE);
+            answerButton.setVisibility(View.INVISIBLE);
+            nextCardButton.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
      * Advances to next flashcard.
      */
     public void onClickNextCard(View view) {
-        flashcardGraphic.setVisibility(View.VISIBLE);
-        answerText.setVisibility(View.INVISIBLE);
+        if(letterFirst){
+            handSignGraphic.setVisibility(View.INVISIBLE);
+            englishLetter.setVisibility(View.VISIBLE);
+            answerButton.setVisibility(View.INVISIBLE);
+            nextCardButton.setVisibility(View.VISIBLE);
+        } else {
+            handSignGraphic.setVisibility(View.VISIBLE);
+            englishLetter.setVisibility(View.INVISIBLE);
+            answerButton.setVisibility(View.VISIBLE);
+            nextCardButton.setVisibility(View.INVISIBLE);
+        }
         alphabet.getNextLetter();
         setUpFlashCard();
-        answerButton.setVisibility(View.VISIBLE);
-        nextCardButton.setVisibility(View.GONE);
     }
 
     /**
      * Sets the image of the current flashcard.
      */
     public void setUpFlashCard() {
-        flashcardGraphic.setImageResource(getResources().getIdentifier("@drawable/" + alphabet.getCurrentLetter(), null, getPackageName()));
-        answerText.setText(alphabet.getCurrentLetterDisplay());
+        handSignGraphic.setImageResource(getResources().getIdentifier("@drawable/" + alphabet.getCurrentLetter(), null, getPackageName()));
+        englishLetter.setText(alphabet.getCurrentLetterDisplay());
     }
+
+
 }
