e372d84b7633cdff4f61189f4dfdc337e212e266
jlufrano <47507303+jlufrano@users.noreply.github.com>
2020-06-18 05:07:01 -0400

    Shake to shuffle update.

---
+++ CS683ProjectReport.md
@@ -0,0 +1,10 @@
+# ASL Fingerspelling Bee
+
+Please see the Word Document uploaded to Blackboard entitled
+"CS683_LufranoJamie_ASLFingerspellingBee_iter4.docx" for my Project
+Report.
+
+My code repository can be found at:  
+https://github.com/ProfRusso/cs683-project-jlufrano
+
+
---
+++ app/src/main/java/edu/bu/metcs/activitylifecycle/MainMenuActivity.java
@@ -0,0 +1,63 @@
+/**
+ * Jamie Lynn Lufrano - ASL Fingerspelling Bee - Project Iteration 1
+ * Activity for Main Menu.
+ */
+
+package edu.bu.metcs.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main_menu);
+    }
+
+    /**
+     * Starts AlphabetChartActivity intent.
+     */
+    public void alphabetChartClick(View view) {
+        Intent intent = new Intent(this, AlphabetChartActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts FlashcardActivity intent.
+     */
+    public void flashcardClick(View view) {
+        Intent intent = new Intent(this, FlashcardActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts ASL Beginner Fingerspelling Bee intent.
+     */
+    public void beginnerSpellingClick(View view) {
+        Intent intent = new Intent(this, BeginnerSpellingBeeActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts ASL Letter Quiz intent.
+     */
+    public void letterQuizClick(View view) {
+        Intent intent = new Intent(this, LetterQuizActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Starts ASL Intermediate Fingerspelling Bee intent.
+     */
+    public void intermediateSpellingClick(View view) {
+        Intent intent = new Intent(this, IntermediateSpellingBeeActivity.class);
+        startActivity(intent);
+    }
+
+
+}
