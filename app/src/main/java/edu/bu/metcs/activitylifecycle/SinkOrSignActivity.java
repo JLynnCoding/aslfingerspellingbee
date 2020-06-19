cb26a1da61414f8b856c738bf41135e6d3537700
jlufrano <47507303+jlufrano@users.noreply.github.com>
2020-06-19 06:02:25 -0400

    Sink or Sign Layout corrected.

--- app/src/main/java/edu/bu/metcs/activitylifecycle/SinkOrSignActivity.java
+++ app/src/main/java/edu/bu/metcs/activitylifecycle/SinkOrSignActivity.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SinkOrSignActivity extends AppCompatActivity {
 
@@ -17,6 +19,10 @@ public class SinkOrSignActivity extends AppCompatActivity {
     private SpellingWords spellingWords;
     private String correctWord;
     private String spellingWord;
+
+    private ImageView letterGraphic1, letterGraphic2, letterGraphic3, letterGraphic4,
+            letterGraphic5, letterGraphic6, letterGraphic7, letterGraphic8, letterGraphic9,
+            letterGraphic10, letterGraphic11, letterGraphic12, letterGraphic13;
 
     int wrongGuesses;
     int replacedLetters;
@@ -28,10 +34,39 @@ public class SinkOrSignActivity extends AppCompatActivity {
 
         spellingWords = new SpellingWords(this,"sinkOrSign");
 
+        letterGraphic1 = (ImageView) findViewById(R.id.letterGraphic1);
+        letterGraphic2 = (ImageView) findViewById(R.id.letterGraphic2);
+        letterGraphic3 = (ImageView) findViewById(R.id.letterGraphic3);
+        letterGraphic4 = (ImageView) findViewById(R.id.letterGraphic4);
+        letterGraphic5 = (ImageView) findViewById(R.id.letterGraphic5);
+        letterGraphic6 = (ImageView) findViewById(R.id.letterGraphic6);
+        letterGraphic7 = (ImageView) findViewById(R.id.letterGraphic7);
+        letterGraphic8 = (ImageView) findViewById(R.id.letterGraphic8);
+        letterGraphic9 = (ImageView) findViewById(R.id.letterGraphic9);
+        letterGraphic10 = (ImageView) findViewById(R.id.letterGraphic10);
+        letterGraphic11 = (ImageView) findViewById(R.id.letterGraphic11);
+        letterGraphic12 = (ImageView) findViewById(R.id.letterGraphic12);
+        letterGraphic13 = (ImageView) findViewById(R.id.letterGraphic13);
+
+        letterGraphic1.setVisibility(View.INVISIBLE);
+        letterGraphic2.setVisibility(View.INVISIBLE);
+        letterGraphic3.setVisibility(View.INVISIBLE);
+        letterGraphic4.setVisibility(View.INVISIBLE);
+        letterGraphic5.setVisibility(View.INVISIBLE);
+        letterGraphic6.setVisibility(View.INVISIBLE);
+        letterGraphic7.setVisibility(View.INVISIBLE);
+        letterGraphic8.setVisibility(View.INVISIBLE);
+        letterGraphic9.setVisibility(View.INVISIBLE);
+        letterGraphic10.setVisibility(View.INVISIBLE);
+        letterGraphic11.setVisibility(View.INVISIBLE);
+        letterGraphic12.setVisibility(View.INVISIBLE);
+        letterGraphic13.setVisibility(View.INVISIBLE);
+
         wrongGuesses = 0;
         replacedLetters = 0;
 
         getWord();
+        setUpWord();
     }
 
     /**
@@ -47,5 +82,88 @@ public class SinkOrSignActivity extends AppCompatActivity {
     /**
      * Sets up images for word and updates as needed.
      */
+    public void setUpWord() {
+        for(int i = 0; i < spellingWord.length(); i++) {
+            if (i == 0){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic1.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic1.setVisibility(View.VISIBLE);
+            } else if (i == 1){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic2.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic2.setVisibility(View.VISIBLE);
+            } else if (i == 2){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic3.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic3.setVisibility(View.VISIBLE);
+            } else if (i == 3){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic4.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic4.setVisibility(View.VISIBLE);
+            } else if (i == 4){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic5.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic5.setVisibility(View.VISIBLE);
+            } else if (i == 5){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic6.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic6.setVisibility(View.VISIBLE);
+            } else if (i == 6){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic7.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic7.setVisibility(View.VISIBLE);
+            } else if (i == 7){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic8.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic8.setVisibility(View.VISIBLE);
+            } else if (i == 8){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic9.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic9.setVisibility(View.VISIBLE);
+            } else if (i == 9){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic10.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic10.setVisibility(View.VISIBLE);
+            } else if (i == 10){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic11.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic11.setVisibility(View.VISIBLE);
+            } else if (i == 11){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic12.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic12.setVisibility(View.VISIBLE);
+            } else if (i == 12){
+                if(("" + spellingWord.charAt(i)).equals(" ")) {
+                    letterGraphic13.setImageResource(getResources().getIdentifier(
+                            "@drawable/space", null, getPackageName()));
+                }
+                letterGraphic13.setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
 }
