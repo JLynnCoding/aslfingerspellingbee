16945d92f186b6f0b25c7ce6240e881cd9072295
jlufrano <47507303+jlufrano@users.noreply.github.com>
2020-06-19 00:08:46 -0400

    Fixed Alphabet Cart Layout and created "Sink or Sign" activity.

---
+++ app/src/main/java/edu/bu/metcs/activitylifecycle/SinkOrSignActivity.java
@@ -0,0 +1,14 @@
+package edu.bu.metcs.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinkOrSignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sink_or_sign);
+    }
+}
